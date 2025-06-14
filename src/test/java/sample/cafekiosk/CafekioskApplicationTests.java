914e7a21e632b6f1aa393f9c0b533486814e9393
yjson <handyejee14@gmail.com>
2025-06-15 00:53:33 +0900

    chore : git 연동 , persistent layer 테스트 코드 추가

---
+++ src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,15 @@
+package sample.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootTest
+class CafekioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
